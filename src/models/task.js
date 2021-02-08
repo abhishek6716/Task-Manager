198df00b4f8aecfcab2bc0b0890f32f3be6370d5
Abhishek singh <abhisheksingh6716@gmail.com>
2021-02-08 20:17:40 +0530

    implementing db

---
+++ src/models/task.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
+
+
+
+
+
+
+// const T1 = new Task({
+//     description: '  GYM',
+// })
+
+// T1.save().then(() => {
+//     console.log(T1)
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
