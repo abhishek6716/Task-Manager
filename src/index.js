3ac3aca36dfa450a569fb9a6213e31e6366fab5a
Abhishek singh <abhisheksingh6716@gmail.com>
2021-02-09 20:32:45 +0530

    refactoring user routes

--- src/index.js
+++ src/index.js
@@ -4,60 +4,25 @@ const User = require('./models/user')
 const Task = require('./models/task')
 const { ObjectID } = require('mongodb')
 const { updateOne } = require('./models/user')
+const userRouter = require('./router/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter)
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
 
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
 
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-})
+// const router = new express.Router()
+// router.get('/test', (req, res) => {
+//     res.send('This is from other router')
+// })
+// app.use(router)
 
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
 
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
 
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -76,44 +41,7 @@ app.post('/tasks', async (req, res) => {
     // })
 })
 
-app.get('/users', async (req, res) => {
 
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
 
 app.get('/tasks', async (req, res) => {
     try {
