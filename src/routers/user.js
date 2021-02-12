faf3c6c5c40bc9d15c9eb1c9d57cd8895f568684
Abhishek singh <abhisheksingh6716@gmail.com>
2021-02-12 12:24:30 +0530

    setting up tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,7 +11,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -26,7 +27,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
