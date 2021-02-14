5f859ee808ed2570bf5cd3639dcacf6991dc516a
Abhishek singh <abhisheksingh6716@gmail.com>
2021-02-14 23:42:53 +0530

    timestamps and filtering data

--- src/index.js
+++ src/index.js
@@ -70,17 +70,17 @@ app.listen(port, () => {
 // }
 // console.log(JSON.stringify(pet))
 
-const Task = require('./models/task')
-const User = require('./models/user')
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('6027e2a8cb58372b20e8d25e')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+// const main = async () => {
+//     // const task = await Task.findById('6027e2a8cb58372b20e8d25e')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
 
-    const user = await User.findById('6027dcd681ac4a33fc982476')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
+//     const user = await User.findById('6027dcd681ac4a33fc982476')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
 
-main()
+// main()
