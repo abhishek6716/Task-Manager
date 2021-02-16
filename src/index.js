ab8d3dcb04f1a61a36624564f0cb46c44cba5890
Abhishek singh <abhisheksingh6716@gmail.com>
2021-02-16 09:59:50 +0530

    uploading profile image

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
 //         res.send('GET requests are disabled')
@@ -84,3 +86,24 @@ app.listen(port, () => {
 // }
 
 // main()
+
+// const multer = require('multer')
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('please upload a word document!'))
+//         }
+//         cb(undefined, true)
+//     }
+// })
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send()
+// }, (error, req, res, next) => {
+//     res.status(400).send({ error: error.message })
+// })
+
+
